e8fbae3d2a8d1edf474d24dfc22741d75a80d0fb
GeoffSpielman <geoff.spielman21@gmail.com>
2017-11-05 23:30:51 -0500

    Changed yaw positioning to reference last rest point, increased accuracy empirically

--- Streaming_Camera/Streaming_Camera.ino
+++ Streaming_Camera/Streaming_Camera.ino
@@ -8,6 +8,7 @@ const int MIN_PITCH = 760;
 const int REST_PITCH = 1430;
 const int INTERVAL_PITCH = 20;
 const int INCREMENT_PITCH = 1;
+const double PITCH_DEGREES_PER_MICROSECOND = 0.11;
 int curPitch = 1430;
 
 //most constants are 'good to know' and for future use - not necessary right now
@@ -16,11 +17,12 @@ const int MIN_CCW_YAW = 1508;
 const int MAX_CW_YAW = 1380;
 const int MIN_CW_YAW = 1455;
 const int REST_YAW = 1480;
-const int INCREMENT_YAW = 2;
+const int INCREMENT_YAW = 8;
+const double YAW_DEGREES_PER_MICROSECOND = 0.005294;
 int curYaw = 1480;
-unsigned long yawTimeLastUpdate;
+unsigned long yawMotionTimeStart;
+double yawMotionValueStart;
 bool yawCurrentlySpinning = false;
-float yawCurrentHeading = 0;
 
 unsigned long lastPitchMove = millis();
 String recString = "";
@@ -60,46 +62,46 @@ void loop() {
        lastPitchMove = millis();      
    }
 
-   //Camera yaw -- //Yaw servo takes 47.47 seconds to complete 180° at the current speed (we'll need to recalibrate later)
-    if (cameraCommand == "Left" || cameraCommand == "Up-Left" || cameraCommand == "Down-Left"){
-      if(!yawCurrentlySpinning){
-        curYaw = MIN_CCW_YAW + INCREMENT_YAW;
-        yawTimeLastUpdate = millis();
-        yawCurrentlySpinning = true;
-      }
-      valuesHaveChanged = true;
-      yawCurrentHeading += (millis() - yawTimeLastUpdate)*180/47470.0;
-      yawTimeLastUpdate = millis();
-    }
-    else if (cameraCommand == "Right" || cameraCommand == "Up-Right" || cameraCommand == "Down-Right"){
-      if(!yawCurrentlySpinning){
-            curYaw = MIN_CW_YAW - INCREMENT_YAW;
-            yawTimeLastUpdate = millis();
-            yawCurrentlySpinning = true;
-      }
-      valuesHaveChanged = true;
-      yawCurrentHeading -= (millis() - yawTimeLastUpdate)*180/47470.0;
-      yawTimeLastUpdate = millis();  
-    }
-    else{
-      curYaw = REST_YAW;
-      yawCurrentlySpinning = false;
-    }
-    
+
+     //Camera yaw
     if (previousCameraCommand != cameraCommand){
-      yawServo.writeMicroseconds(curYaw);
+        if (cameraCommand == "Left" || cameraCommand == "Up-Left" || cameraCommand == "Down-Left"){
+            curYaw = MIN_CCW_YAW + INCREMENT_YAW;
+            yawMotionTimeStart = millis();
+            yawCurrentlySpinning = true;
+        }
+        else if (cameraCommand == "Right" || cameraCommand == "Up-Right" || cameraCommand == "Down-Right"){
+            curYaw = MIN_CW_YAW - INCREMENT_YAW;
+            yawMotionTimeStart = millis();
+            yawCurrentlySpinning = true;
+        }
+        else{
+            curYaw = REST_YAW;
+            yawCurrentlySpinning = false;
+            //Yaw servo takes 34 seconds to complete 180° at the current speed (we'll need to recalibrate later)
+            yawMotionValueStart = (millis() - yawMotionTimeStart)*180/34000.0;
+        }
+        yawServo.writeMicroseconds(curYaw);
+        previousCameraCommand = cameraCommand;
     }
     
- 
-
-
    //provide feedback to UI
-   if (valuesHaveChanged || yawCurrentlySpinning){
+   if (valuesHaveChanged || yawCurrentlySpinning){    
       //Right now 1430 seems level - I'll call that 0 degrees for now but we need to recalibrate later
       //Pitch servo: 0.079°/μsec according to data sheet
-      Serial.print((curPitch - 1430)*0.079, 4);
+      Serial.print((curPitch - 1430)*PITCH_DEGREES_PER_MICROSECOND, 4);
       Serial.print(",");
-      Serial.print(yawCurrentHeading, 4);
+      if (yawCurrentlySpinning){
+          if(curYaw == MIN_CCW_YAW + INCREMENT_YAW){
+            Serial.print(yawMotionValueStart + ((millis() - yawMotionTimeStart)*YAW_DEGREES_PER_MICROSECOND), 4);
+          }
+          else{
+            Serial.print(yawMotionValueStart - ((millis() - yawMotionTimeStart)*YAW_DEGREES_PER_MICROSECOND), 4);
+          }
+      }
+      else{
+        Serial.print(yawMotionValueStart, 4); 
+      }
       Serial.println("|");
       valuesHaveChanged = false;
    }
