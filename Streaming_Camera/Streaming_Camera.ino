7d7915a5f4b0811ddca22b96755c514a1e89904b
GeoffSpielman <geoff.spielman21@gmail.com>
2017-11-05 22:26:01 -0500

    changed yaw values to incremental (i.e. based on a few milliseconds at a time)

--- Streaming_Camera/Streaming_Camera.ino
+++ Streaming_Camera/Streaming_Camera.ino
@@ -1,15 +1,14 @@
 #include <Servo.h>
-const int LED = 13;
 const int PITCH_SERVO = 9;
 const int YAW_SERVO = 6;
 
 
 const int MAX_PITCH = 2230; 
 const int MIN_PITCH = 760;
-const int REST_PITCH = 1495;
+const int REST_PITCH = 1430;
 const int INTERVAL_PITCH = 20;
 const int INCREMENT_PITCH = 1;
-int curPitch = 1495;
+int curPitch = 1430;
 
 //most constants are 'good to know' and for future use - not necessary right now
 const int MAX_CCW_YAW = 1600;
@@ -17,13 +16,13 @@ const int MIN_CCW_YAW = 1508;
 const int MAX_CW_YAW = 1380;
 const int MIN_CW_YAW = 1455;
 const int REST_YAW = 1480;
-const int INCREMENT_YAW = 1;
+const int INCREMENT_YAW = 2;
 int curYaw = 1480;
+unsigned long yawTimeLastUpdate;
+bool yawCurrentlySpinning = false;
+float yawCurrentHeading = 0;
 
-
-int lastLEDSwitch = millis();
 unsigned long lastPitchMove = millis();
-boolean lightOn = true;
 String recString = "";
 String cameraCommand = "";
 String previousCameraCommand = "initialization";
@@ -33,7 +32,6 @@ Servo pitchServo;
 Servo yawServo;
 void setup() {
   Serial.begin(9600);  
-  pinMode(LED, OUTPUT);
   pitchServo.attach(PITCH_SERVO); 
   pitchServo.writeMicroseconds(REST_PITCH);
   yawServo.attach(YAW_SERVO);
@@ -43,17 +41,6 @@ void setup() {
 
 void loop() {
 
-   //LED heartbeat
-   if ((millis() - lastLEDSwitch) > 1000){
-     lastLEDSwitch = millis();
-     if (lightOn){
-       digitalWrite(LED, LOW);
-     }
-     else{
-       digitalWrite(LED, HIGH);
-     }
-     lightOn = !lightOn;
-   }
 
    //Camera pitch
    if ((cameraCommand != "Idle") && (millis() - lastPitchMove > INTERVAL_PITCH)){
@@ -73,26 +60,47 @@ void loop() {
        lastPitchMove = millis();      
    }
 
-   //Camera yaw
-   if (cameraCommand != previousCameraCommand){
-      if (cameraCommand == "Left" || cameraCommand == "Up-Left" || cameraCommand == "Down-Left"){
+   //Camera yaw -- //Yaw servo takes 47.47 seconds to complete 180° at the current speed (we'll need to recalibrate later)
+    if (cameraCommand == "Left" || cameraCommand == "Up-Left" || cameraCommand == "Down-Left"){
+      if(!yawCurrentlySpinning){
         curYaw = MIN_CCW_YAW + INCREMENT_YAW;
-        valuesHaveChanged = true;
+        yawTimeLastUpdate = millis();
+        yawCurrentlySpinning = true;
       }
-      else if (cameraCommand == "Right" || cameraCommand == "Up-Right" || cameraCommand == "Down-Right"){
-        curYaw = MIN_CW_YAW - INCREMENT_YAW;
-        valuesHaveChanged = true;
+      valuesHaveChanged = true;
+      yawCurrentHeading += (millis() - yawTimeLastUpdate)*180/47470.0;
+      yawTimeLastUpdate = millis();
+    }
+    else if (cameraCommand == "Right" || cameraCommand == "Up-Right" || cameraCommand == "Down-Right"){
+      if(!yawCurrentlySpinning){
+            curYaw = MIN_CW_YAW - INCREMENT_YAW;
+            yawTimeLastUpdate = millis();
+            yawCurrentlySpinning = true;
       }
-      else{
-        curYaw = REST_YAW;
-      }
-      previousCameraCommand = cameraCommand;
+      valuesHaveChanged = true;
+      yawCurrentHeading -= (millis() - yawTimeLastUpdate)*180/47470.0;
+      yawTimeLastUpdate = millis();  
+    }
+    else{
+      curYaw = REST_YAW;
+      yawCurrentlySpinning = false;
+    }
+    
+    if (previousCameraCommand != cameraCommand){
       yawServo.writeMicroseconds(curYaw);
-   }
+    }
+    
+ 
+
 
    //provide feedback to UI
-   if (valuesHaveChanged){
-      Serial.println(String(curPitch) + "," + String(5) + "|");
+   if (valuesHaveChanged || yawCurrentlySpinning){
+      //Right now 1430 seems level - I'll call that 0 degrees for now but we need to recalibrate later
+      //Pitch servo: 0.079°/μsec according to data sheet
+      Serial.print((curPitch - 1430)*0.079, 4);
+      Serial.print(",");
+      Serial.print(yawCurrentHeading, 4);
+      Serial.println("|");
       valuesHaveChanged = false;
    }
 }
